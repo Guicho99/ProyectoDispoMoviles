093ab45f5328fa7c98e62b9470d3e526f1e2ea6e
Mauricio Rangel <guicho99@live.com.mx>
2023-10-09 22:51:58 -0600

    Actualizacion 1.2

---
+++ app/src/main/java/com/example/inventario/OrdenesDeCompras.java
@@ -0,0 +1,14 @@
+package com.example.inventario;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class OrdenesDeCompras extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ordenes_de_compra);
+    }
+}
